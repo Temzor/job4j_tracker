a64ac694a88435b98f553a79772bdb9f952db593
Капустин Дмитрий <onewaykd@gmail.com>
2021-05-16 10:10:37 +0300

    1.3.3. Map. HashMap. LinkedHashMap  6. Вычисление значений в отображении если ключа не существует

---
+++ src/main/java/ru/job4j/map/ComputeIfPresent.java
@@ -0,0 +1,14 @@
+package ru.job4j.map;
+
+import java.util.Map;
+
+public class ComputeIfPresent {
+    public static Map<Integer, String> collectData(
+            Map<Integer, String> name,
+            Map<Integer, String> surname) {
+        for (Integer index : name.keySet()) {
+            name.computeIfPresent(index, (key, value) -> value + " " + surname.get(index));
+        }
+        return name;
+    }
+}
