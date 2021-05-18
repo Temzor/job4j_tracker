96c1bdfc9cbb105e42fce30dd7f97046609c93d3
Temzor <onewaykd@gmail.com>
2021-05-18 17:04:03 +0300

    1. Уровень - Стажер  Блок 3. Collections. Lite.  1. Коллекция List, ArrayList.  3. Конвертация ArrayList в двухмерный массив

---
+++ src/main/java/ru/job4j/practice/collection/ConvertList2Array.java
@@ -0,0 +1,31 @@
+package ru.job4j.practice.collection;
+
+import java.util.List;
+
+public class ConvertList2Array {
+    public static int[][] toArray(List<Integer> list, int cells) {
+        int groups = (int) Math.ceil((double) list.size() / cells);
+        System.out.println(groups);
+        int[][] array = new int[groups][cells];
+        int row = 0, cell = 0;
+        for (Integer num : list) {
+            array[row][cell++] = num;
+            if (cell == cells) {
+                row++;
+                cell = 0;
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
+        int[][] rsl = toArray(list, 4);
+        for (int[] row : rsl) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
