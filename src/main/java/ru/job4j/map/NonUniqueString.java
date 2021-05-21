1e217db5eb047d28c8fb315c62480d63e555ec19
Капустин Дмитрий <onewaykd@gmail.com>
2021-05-21 17:36:11 +0300

    1.3.3. Map. HashMap. LinkedHashMap  10. Массив строк с повторяющимися значениями.

---
+++ src/main/java/ru/job4j/map/NonUniqueString.java
@@ -0,0 +1,17 @@
+package ru.job4j.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NonUniqueString {
+    public static Map<String, Boolean> checkData(String[] strings) {
+        Map<String, Boolean> map = new HashMap<>();
+        for (String s : strings) {
+            map.computeIfPresent(s, (key, value) -> true);
+            map.putIfAbsent(s, false);
+
+        }
+
+        return map;
+    }
+}
