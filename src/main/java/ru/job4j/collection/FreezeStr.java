ed9ad7bae5cf34a01cba9fdc4121dce2e4e2f1c7
Temzor <onewaykd@gmail.com>
2021-05-24 13:57:11 +0300

    1.3.6. Контрольные вопросы  1. Экзамен. Коллекции.   Решение с помощью HashMap.

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -6,36 +6,19 @@ import java.util.Map;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        /*
-          Comparing the length of two lines, for the zone of use of the code below
-         */
         if (left.length() != right.length()) {
             return false;
         }
 
-        /*
-         * Split words into an array of letters
-         */
         char[] charLeft = left.toCharArray();
         char[] charRight = right.toCharArray();
 
-        /*
-         * Sort letters with natural order
-         */
         Arrays.sort(charLeft);
         Arrays.sort(charRight);
-
-        /*
-         * Create HashMap with <K, V> =  <String, Integer>, String = char letter,
-         * Integer = count(negligible)
-         */
 
         Map<Character, Integer> map1 = new HashMap<>();
         Map<Character, Integer> map2 = new HashMap<>();
 
-        /*
-         * Filling in the Map
-         */
         for (int i = 0; i < charLeft.length; i++) {
             map1.put(charLeft[i], i);
             map2.put(charRight[i], i);
