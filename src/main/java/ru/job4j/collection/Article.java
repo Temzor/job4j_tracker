66673b3c185b65edc9bf281d2cc8c83f9d687caa
Temzor <onewaykd@gmail.com>
2021-05-25 11:44:35 +0300

    1.3.6. Контрольные вопросы  1. Экзамен. Коллекции.   Решение с помощью HashSet.

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -6,10 +6,13 @@ import java.util.Set;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        String clearOrigin = origin.replaceAll("(?U)[^\\p{L}\\p{N}\\s]+", "");
+        String clearOrigin = origin.replaceAll("\\p{P}", "");
         Set<String> setOrigin = new HashSet<>(Arrays.asList(clearOrigin.split(" ")));
-        Set<String> setLine = new HashSet<>(Arrays.asList(line.split(" ")));
-
-        return setOrigin.containsAll(setLine);
+        for (String s : line.split(" ")) {
+            if (!setOrigin.contains(s)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
