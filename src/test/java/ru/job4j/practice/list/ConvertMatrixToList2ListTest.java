26d06a93d025b2b11e8990d606b24a50340be13e
Капустин Дмитрий <onewaykd@gmail.com>
2021-06-04 17:25:53 +0300

    5. Преобразование матрицы чисел в список чисел. [#110228]

--- src/test/java/ru/job4j/practice/list/ConvertMatrixToList2ListTest.java
+++ src/test/java/ru/job4j/practice/list/ConvertMatrixToList2ListTest.java
@@ -1,14 +1,13 @@
-package ru.job4j.list;
+package ru.job4j.practice.list;
 
+import org.junit.Ignore;
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.List;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class ConvertMatrix2ListTest {
+public class ConvertMatrixToList2ListTest {
     @Test
     public void when2on2ArrayThenList4() {
         ConvertMatrix2List list = new ConvertMatrix2List();
