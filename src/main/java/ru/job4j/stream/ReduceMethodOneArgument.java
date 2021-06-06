49147b78fc09a0032d8d15e39c354d19ab00215e
Капустин Дмитрий <onewaykd@gmail.com>
2021-06-06 15:23:19 +0300

    0.3. Понятие редукции. Метод reduce(). Произведение чисел

---
+++ src/main/java/ru/job4j/stream/ReduceMethodOneArgument.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ReduceMethodOneArgument {
+    public static Integer reduce(List<Integer> list) {
+        return list
+                .stream()
+                .reduce((n1, n2) -> n1 * n2)
+                .get();
+    }
+}
