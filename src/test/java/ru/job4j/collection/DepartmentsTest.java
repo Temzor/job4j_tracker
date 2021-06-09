3a5e739a096a837c8a75896a83167df91085bfd2
Капустин Дмитрий <onewaykd@gmail.com>
2021-06-09 22:23:21 +0300

    1. Метод .of [#70623 #166357]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -1,12 +1,15 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 public class DepartmentsTest {
+
     @Test
     public void whenMissed() {
         List<String> input = List.of("k1/sk1");
@@ -21,55 +24,5 @@ public class DepartmentsTest {
         List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
-    }
-
-    @Test
-    public void whenHasNotK2department() {
-        List<String> deps = List.of(
-                "K2/SK1"
-        );
-        boolean expected = Departments.fillGaps(deps).contains("K2");
-        assertThat(expected, is(true));
-    }
-
-    @Test
-    public void whenDepartmentsSortAsc() {
-        List<String> deps = List.of(
-                "K2",
-                "K2/SK1",
-                "K1",
-                "K1/SK1/SSK2",
-                "K1/SK2"
-        );
-
-        List<String> expected = List.of(
-                "K1",
-                "K1/SK1/SSK2",
-                "K1/SK2",
-                "K2",
-                "K2/SK1"
-        );
-        Departments.sortAsc(deps);
-        assertThat(deps, is(expected));
-    }
-
-    @Test
-    public void whenDepartmentsSortDesc() {
-        List<String> deps = List.of(
-                "K1",
-                "K2",
-                "K1/SK1/SSK2",
-                "K1/SK2",
-                "K2/SK1"
-        );
-        List<String> expected = List.of(
-                "K2",
-                "K2/SK1",
-                "K1",
-                "K1/SK1/SSK2",
-                "K1/SK2"
-        );
-        Departments.sortDesc(deps);
-        assertThat(deps, is(expected));
     }
 }
