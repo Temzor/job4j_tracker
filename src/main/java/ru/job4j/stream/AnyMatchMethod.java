4b1fc20ff0e935437f9f917af1ef53cc60b8ddb4
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-02 14:41:40 +0300

    4.3. Терминальные операции. Метод anyMatch()

---
+++ src/main/java/ru/job4j/stream/AnyMatchMethod.java
@@ -0,0 +1,11 @@
+package ru.job4j.stream;
+
+import java.util.List;
+
+public class AnyMatchMethod {
+    public static boolean check(List<String> data) {
+        return data
+                .stream()
+                .anyMatch(d -> d.equals("job4j"));
+    }
+}
