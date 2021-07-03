bc75ca98757bc8a0690d6d90129b592f29397b5f
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-03 15:46:27 +0300

    2.2. Fuction. Получить корень квадратный из числа

--- src/main/java/ru/job4j/lambda/FunctionSqrt.java
+++ src/main/java/ru/job4j/lambda/FunctionSqrt.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 public class FunctionSqrt {
 
     public static double calculate(double x) {
-        return calculate(c -> Math.sqrt(c), x);
+        return calculate(Math::sqrt, x);
     }
 
     public static double calculate(Function<Double, Double> y, double x) {
