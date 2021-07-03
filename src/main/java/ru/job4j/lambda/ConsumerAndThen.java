2f89c80976298c8d0001f5c6d2650c13e72ad68d
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-03 15:52:43 +0300

    3.1. Consumer. Вывод строки. Метод andThen()

--- src/main/java/ru/job4j/lambda/ConsumerAndThen.java
+++ src/main/java/ru/job4j/lambda/ConsumerAndThen.java
@@ -4,8 +4,8 @@ import java.util.function.Consumer;
 
 public class ConsumerAndThen {
     public static Consumer<String> consumer(String input) {
-         Consumer<String> print = (p -> System.out.print(p));
-         Consumer<String> ln = (p -> System.out.println());
+         Consumer<String> print = n -> System.out.print(input);
+         Consumer<String> ln = n -> System.out.println();
          return print.andThen(ln);
     }
 }
