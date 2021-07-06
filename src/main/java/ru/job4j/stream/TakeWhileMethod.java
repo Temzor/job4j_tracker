22c48f350f0ac50b6305cb118277a52094ee1b9b
Temzor <onewaykd@gmail.com>
2021-07-06 18:34:53 +0300

    9.3. Промежуточные операции. Метод takeWhile()

---
+++ src/main/java/ru/job4j/stream/TakeWhileMethod.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TakeWhileMethod {
+    public static List<Integer> takeWhile(List<Integer> numbers) {
+        return numbers
+                .stream()
+                .sorted()
+                .takeWhile(x -> x < 0)
+                .collect(Collectors.toList());
+    }
+}
