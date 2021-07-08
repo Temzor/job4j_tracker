cdc13cd243d839d7da54a2c3594fffaa082e8a7f
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-08 10:06:59 +0300

    Is this a triangle? https://www.codewars.com/kata/56606694ec01347ce800001b/train/java

---
+++ src/test/java/ru/training/codewars/TriangleTesterTest.java
@@ -0,0 +1,12 @@
+package ru.training.codewars;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TriangleTesterTest {
+    @Test
+    public void publicTests() {
+        assertTrue(TriangleTester.isTriangle(1, 2, 2));
+        assertFalse(TriangleTester.isTriangle(7, 2, 2));
+    }
+}
