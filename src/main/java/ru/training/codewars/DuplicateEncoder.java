62653f20362ac0ea85b4e3b53d20bf01de03b0eb
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-08 10:20:00 +0300

    Duplicate Encoder https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/java

--- src/main/java/ru/training/codewars/DuplicateEncoder.java
+++ src/main/java/ru/training/codewars/DuplicateEncoder.java
@@ -1,13 +1,20 @@
 package ru.training.codewars;
 
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 public class DuplicateEncoder {
     static String encode(String word) {
-        word = word.toLowerCase();
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < word.length(); ++i) {
-            char c = word.charAt(i);
-            result.append(word.lastIndexOf(c) == word.indexOf(c) ? "(" : ")");
-        }
-        return result.toString();
+        return word
+                .toLowerCase()
+                .chars()
+                .mapToObj(x -> String.valueOf((char) x))
+                .map(x -> word
+                        .toLowerCase()
+                        .indexOf(x)
+                        == word
+                        .toLowerCase()
+                        .lastIndexOf(x) ? "(" : ")")
+                .collect(Collectors.joining());
     }
 }
