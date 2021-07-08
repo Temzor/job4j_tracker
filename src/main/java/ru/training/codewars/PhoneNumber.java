843d48340317ed6b3dab0882954927f3dbe3d44b
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-08 11:39:00 +0300

    Create Phone Number https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/java

--- src/main/java/ru/training/codewars/PhoneNumber.java
+++ src/main/java/ru/training/codewars/PhoneNumber.java
@@ -2,26 +2,8 @@ package ru.training.codewars;
 
 public class PhoneNumber {
     public static String createPhoneNumber(int[] numbers) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < numbers.length; i++) {
-            if (i < 3) {
-                if (i == 0) {
-                    result.append("(").append(numbers[i]);
-                } else if (i == 2) {
-                    result.append(") ");
-                } else {
-                    result.append(numbers[i]).append(numbers[2]);
-                }
-            } else if (i < 6) {
-                if (i == 5) {
-                    result.append(numbers[5]).append("-");
-                } else {
-                    result.append(numbers[i]);
-                }
-            } else {
-                result.append(numbers[i]);
-            }
-        }
-        return String.valueOf(result);
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d", numbers[0], numbers[1], numbers[2],
+                 numbers[3], numbers[4], numbers[5], numbers[6],
+                numbers[7], numbers[8], numbers[9]);
     }
 }
