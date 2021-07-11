96afd754e747b61c542681b8119ea74c6effe847
Капустин Дмитрий <onewaykd@gmail.com>
2021-07-11 16:19:09 +0300

    2. Аттестация [#49777]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -22,18 +23,16 @@ public class Analyze {
                 .map(pupil -> new Tuple(pupil.getName(), pupil.getSubjects().stream()
                         .mapToInt(Subject::getScore)
                         .average().orElse(-1D)))
-                .sorted(Comparator.comparing(Tuple::getName))
                 .collect(Collectors.toList());
     }
 
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
         return stream
                 .flatMap(pupil -> pupil.getSubjects().stream())
-                .collect(Collectors.groupingBy(Subject::getName,
+                .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new,
                         Collectors.averagingDouble(Subject::getScore)))
                 .entrySet().stream()
                 .map(subject -> new Tuple(subject.getKey(), subject.getValue()))
-                .sorted(Comparator.comparing(Tuple::getName).reversed())
                 .collect(Collectors.toList());
     }
 
